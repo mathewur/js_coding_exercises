39849a41108a9912c7885c461a7bff2c5e8d242f
mathewur <78866876+mathewur@users.noreply.github.com>
2021-03-04 18:43:11 +0000

    exercise005 getWordFrequencies yet to complete

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,36 +1,63 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  let isLastIndex = nums.indexOf(n) === nums.length - 1;
+  let isNotInArray = nums.indexOf(n) < 0;
+
+  //console.log("isLastIndex"+isLastIndex);
+  return isLastIndex || isNotInArray ? null : nums[nums.indexOf(n) + 1];
 };
 
-const count1sand0s = str => {
+const count1sand0s = (str) => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  var sum = 0;
+
+  for (var i = 0, length = str.length; i < length; i++) {
+    sum += Number(str[i]);
+  }
+
+  return {
+    1: str.length - sum,
+    0: sum,
+  };
 };
 
-const reverseNumber = n => {
+const reverseNumber = (n) => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  return parseInt(n.toString().split("").reverse().join(""));
 };
 
-const sumArrays = arrs => {
+const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  return arrs.flat().reduce((a, b) => a + b);
 };
 
-const arrShift = arr => {
+const arrShift = (arr) => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  console.log("before pop arr" + arr);
+  let arrbefore = arr;
+  let arrLast = arr.pop(); // removes last element and changes arr
+  let arrFirst = arr.shift(); // removes first element and changes arr
+  console.log("arrayLastElement" + arrLast + " has popped out " + arr);
+  arr.splice(0, 0, arrLast);
+  arr.push(arrFirst);
+
+  console.log("after add arr" + arr);
+  return arrbefore.length < 2 ? arrbefore : arr;
 };
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  let hayStackValues = Object.values(haystack);
+  return (
+    hayStackValues.findIndex((hayItem) =>
+      hayItem.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    ) >= 0
+  );
 };
 
-const getWordFrequencies = str => {
+const getWordFrequencies = (str) => {
   if (str === undefined) throw new Error("str is required");
   // Your code here!
 };
@@ -42,5 +69,5 @@ module.exports = {
   sumArrays,
   arrShift,
   findNeedle,
-  getWordFrequencies
+  getWordFrequencies,
 };
